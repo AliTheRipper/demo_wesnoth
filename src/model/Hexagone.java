b24caf8ebb1722752f9a8d5c1e7237556b363930
hajar-khettou <khettouhajar@gmail.com>
2025-05-05 02:00:39 +0200

    Ajout de Unite

--- src/model/Hexagone.java
+++ src/model/Hexagone.java
@@ -4,26 +4,43 @@ public class Hexagone {
     private int x;
     private int y;
     private TypeTerrain typeTerrain;
-    private boolean isVisible = false;
+    private Unite unite;
+    private boolean visible = false;
 
     public Hexagone(int x, int y, TypeTerrain typeTerrain) {
         this.x = x;
         this.y = y;
         this.typeTerrain = typeTerrain;
+        this.unite = null;
     }
 
     public int getX() { return x; }
+
     public int getY() { return y; }
+
     public TypeTerrain getTypeTerrain() { return typeTerrain; }
 
-    public boolean isVisible() { return isVisible; }
-    public void setVisible(boolean visible) { this.isVisible = visible; }
+    public Unite getUnite() { return unite; }
 
-    public int getCoutDeplacement() {
-        return typeTerrain.getCoutDeplacement();
-    }
+    public void setUnite(Unite unite) { this.unite = unite; }
 
     public boolean estAccessible() {
         return typeTerrain != TypeTerrain.EAU_PROFONDE;
+    }
+
+    public double obtenirBonusDefense() {
+        return typeTerrain.getBonusDefense();
+    }
+
+    public int obtenirCoutDeplacement() {
+        return typeTerrain.getCoutDeplacement();
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
     }
 }
